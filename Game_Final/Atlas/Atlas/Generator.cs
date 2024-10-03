6ebc85fd0162144a5a57987e337c7d377d4dbbb7
nmulvaine <nmulvaine@icloud.com>
2024-10-02 21:27:50 -0400

    Add project files.

---
+++ Game_Final/Atlas/Atlas/Generator.cs
@@ -0,0 +1,20 @@
+namespace roomGenerator;
+/// <summary>
+/// Randomly generates rooms with items and hazords
+/// Plugs into for loop for a set number of rooms (maybe based off difficulty)
+/// Items stored will have a percentage chance of finding
+/// </summary>
+public class roomGen
+{
+    
+}
+
+public class itemGen
+{
+
+}
+
+public class hazGen
+{
+    
+}
---
+++ Game_Final/Atlas/Atlas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+
+///Console.WriteLine("Welcome To Atlas.");
+string message = "║";
+
+for (int i = 0; i < 4; i++)
+{
+    Console.Clear();
+    Thread.Sleep(500);
+
+    Console.WriteLine(message);
+    Thread.Sleep(500);
+}
+
+Console.Clear();
+Console.WriteLine("║Hello?");
+Thread.Sleep(1500);
+Console.Clear();
+Thread.Sleep(1500);
+Console.WriteLine("║Is someone there?");
+Thread.Sleep(1500);
+Console.Clear();
+Console.WriteLine("║Please if you're there, answer me. Who is this?");
+Thread.Sleep(2500);
+Console.Clear();
+Console.ReadLine();
+string userName = Console.ReadLine();
+Console.Write("║Hello" + userName, "\r\n║I was designed to help my creator safely go North.", 
+"I can't seem to remember how we got there though... Could you tell me?");
+Thread.Sleep(1500);
